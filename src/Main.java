a26a6440a03312770045f5fe3ae3d84c93a31b01
hc84.shin <dzzwe@naver.com>
2021-08-06 08:52:27 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] numbers = {1,1,1,1,1};
+        int target = 3;
+        int result = s.solution(numbers,target);
+        System.out.println(result);
+    }
+
+    static class Solution {
+        public int solution(int[] numbers, int target) {
+            int answer = 0;
+            //더하기
+            answer += dfs(0, numbers, target, 1, 0);
+            //빼기
+            answer += dfs(0, numbers, target, -1, 0);
+            return answer;
+        }
+        public int dfs(int i, int[] numbers, int target, int chk, int sum) {
+            if(i == numbers.length) {
+                return 0;
+            }
+            sum += numbers[i]*chk;
+            if(sum == target && i == numbers.length-1) {
+                return 1;
+            }else {
+                int result = 0;
+                result += dfs(i+1, numbers, target, 1, sum);
+                result += dfs(i+1, numbers, target, -1, sum);
+                return result;
+            }
+        }
+    }
+
+
+
+}
---
+++ src/여행경로.java
@@ -0,0 +1,62 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+/*
+[["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+* */
+public class 여행경로 {
+    static ArrayList<String> result = new ArrayList<>();
+    static String[] tmp;
+    static int visit[];
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[][] tic = {{"ICN", "B"},
+                          {"B", "ICN"},
+                          {"ICN", "A"},
+                          {"A", "D"},
+                          {"D", "A"}};
+        String[] answer = s.solution(tic);
+        System.out.println(Arrays.asList(answer).toString());
+    }
+
+    static class Solution {
+        public String[] solution(String[][] tickets) {
+            visit = new int[tickets.length]; //티켓사용여부
+            tmp = new String[tickets.length];
+            dfs("ICN",0,tickets);
+
+            //String[] answer = res.toArray(new String[res.size()]);
+            String[] answer = new String[1];
+            return answer;
+        }
+        /*
+        [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+                String[][] tic = {{"ICN", "B"},
+                          {"B", "ICN"},
+                          {"ICN", "A"},
+                          {"A", "D"},
+                          {"D", "A"}};
+        * */
+        public void dfs(String start,int v, String[][] tickets){
+            if(v == tickets.length){
+                result.add(start);
+                if(Arrays.asList(tmp).toString().compareTo(result.toString()) >= -1){
+                    tmp = result.toArray(new String[result.size()]);
+                }
+                return;
+            }
+            for(int i = 0; i < tickets.length; i++){
+                if(visit[i] == 0 && tickets[i][0].equals(start)){
+                    result.add(start);
+                    visit[i] = 1; //티켓 사용
+                    dfs(tickets[i][1],v+1,tickets);
+                    visit[i] = 0; //티켓 미사용
+                    result.remove(result.size()-1);
+                }
+            }
+        }
+
+
+    }
+}
