008896f3941a09175bd94b5a31868476c2283d93
hc84.shin <dzzwe@naver.com>
2021-08-23 00:23:39 +0900

    LeetCode SudokuSolver

--- src/LeetCode/dfs/SudokuSolver.java
+++ src/LeetCode/dfs/SudokuSolver.java
@@ -5,25 +5,25 @@ public class SudokuSolver {
         SudokuSolver s = new SudokuSolver();
         char[][] board = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
         s.solveSudoku(board);
+        System.out.println(board);
     }
 
     public void solveSudoku(char[][] board) {
         backTracking(0,board);
     }
-
     public boolean backTracking(int index, char[][] board){
         if(index > 80){
             return true;
         }
-        int h;
-        int w;
-        if(index == 0){
-            h = 0;
-            w = 0;
-        }else{
-            h = index/9;
-            w = index%9;
-        }
+        int h = index == 0 ? 0 : index/9;
+        int w = index == 0 ? 0 : index%9;
+//        if(index == 0){
+//           // h = 0;
+//            w = 0;
+//        }else{
+//            h = index/9;
+//            w = index%9;
+//        }
         if (board[h][w] == '.') {
             for(int i = 1; i <= 9; i++){
                 board[h][w] = Character.forDigit(i,10);
@@ -31,7 +31,6 @@ public class SudokuSolver {
                     if(backTracking(index+1,board)){
                         return true;
                     }
-                    //board[h][w] = '.';
                 }
             }
             board[h][w] = '.';
@@ -44,15 +43,12 @@ public class SudokuSolver {
     public boolean isValid(int h, int w, char[][] board){
         char checkNumber = board[h][w];
         for(int i = 0; i < 9; i++){
-            //가로체크
             if(i!=w && board[h][i] == checkNumber){
                 return false;
             }
-            //세로체크
             if(i!=h && board[i][w] == checkNumber){
                 return false;
             }
-            //3x3체크
             int checkHeight = i/3 + ((h/3)*3);
             int checkWidth = i%3 + ((w/3)*3);
             if(checkHeight != h && checkWidth != w && board[checkHeight][checkWidth] == checkNumber){
