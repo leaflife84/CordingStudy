de2efd9c05b02b91e39659521217b6c7ba3fe259
hc84.shin <dzzwe@naver.com>
2021-09-25 22:20:29 +0900

    LeetCode backTracking

--- src/LeetCode/backTracking/LetterCombinations.java
+++ src/LeetCode/backTracking/LetterCombinations.java
@@ -5,19 +5,36 @@ import java.util.List;
 
 public class LetterCombinations {
     String alpha[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    private static List<String> list = new ArrayList<>();
     public static void main(String[] args) {
         LetterCombinations l = new LetterCombinations();
-        List<String> list = l.letterCombinations("23");
+        list = l.letterCombinations("");
         System.out.println(list.stream().toList());
 
     }
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        dfs(result, 0, digits);
-        return result;
+        if(digits.length() == 0){
+            return list;
+        }
+        dfs(new char[digits.length()], 0, digits);
+        return list;
 
     }
-    public void dfs(List<String> result, int index, String digits){
+    public void dfs(char[] select, int index, String digits){
+        if(index == digits.length()){
+            StringBuilder s = new StringBuilder();
+            for (char c : select) {
+                s.append(c);
+            }
+            list.add(s.toString());
+            return;
+        }
+        int charIndex = Character.getNumericValue(digits.charAt(index));
+        for (int i = 0; i < alpha[charIndex].length(); i++) {
+            select[index] = alpha[charIndex].charAt(i);
+            dfs(select, index+1, digits);
+        }
+
 
     }
 }
