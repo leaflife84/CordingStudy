c5ce7d939eefb760a1fb2bd86356d24d7e43ba64
hc84.shin <dzzwe@naver.com>
2021-10-03 22:29:33 +0900

    LeetCode wb

--- src/WB_TEST/oct/Test2.java
+++ src/WB_TEST/oct/Test2.java
@@ -21,32 +21,18 @@ public class Test2 {
         list.remove(0);
         list.remove(1);
         int cal = 1;
+        int currentIndex = 1;
         for (int i = 2; i < answer.length; i++) {
             int insertData = i+1; //3
             cal *= 2;
-            cal = cal%(answer.length-count);
-            for (int j = 0; j < list.size(); j++) {
-                if(j != list.size()-1 && index > list.get(j)) continue;
-                else {
-                    int t = 0;
-                    if(cal >= list.size()) {
-                        t = list.size() - j;
-                        if(index > list.get(j)){
-                            t = 1;
-                        }
-                        index = list.get((cal - t)%list.size());
-                        list.remove((cal - t)%list.size());
-                        break;
-                    }else{
-                        index = list.get((j+cal)%list.size());
-                        list.remove((j+cal)%list.size());
-                        break;
-                    }
-                }
-            }
+            cal = cal%(answer.length-1);
+            int listIndex = (currentIndex+cal)%list.size();
+            index = list.get(listIndex);
+            currentIndex = (currentIndex+cal)%list.size();
+            list.remove(listIndex);
             answer[index] = insertData;
         }
-
+        //1,6,2,7,5,3,4 // 1,12,2,11..
         return answer;
     }
 }
